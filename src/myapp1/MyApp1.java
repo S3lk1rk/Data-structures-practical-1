9dd2220e29237bfdd36cd60015bf9c6a99259d39
Matthew <matthew.selkirk51@gmail.com>
2021-01-27 10:14:40 +0000

    Almost done input

--- src/myapp1/MyApp1.java
+++ src/myapp1/MyApp1.java
@@ -5,6 +5,8 @@
  */
 package myapp1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author user
@@ -14,8 +16,39 @@ public class MyApp1 {
     /**
      * @param args the command line arguments
      */
+    
+    public static void printSquare(int [][] theSquare)
+    {
+        for (int )
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        int[][] magicSquare = { {0,0,0},{0,0,0},{0,0,0} };
+        int theRow = 0;
+        int theCol = 0;
+        int theDigit = 0;
+        Scanner userInput = new Scanner(System.in);
+        boolean stop = false;
+        
+            //Main Loop
+            do {
+                printSquare(magicSquare);
+                System.out.print("please enter the row to place the number");
+                theRow = userInput.nextInt();
+                System.out.print("please enter the column to place the number");
+                theCol = userInput.nextInt();
+                System.out.print("please enter the number");
+                theDigit = userInput.nextInt();
+                
+                magicSquare[theRow][theCol] = theDigit;
+                stop = checkSquare(magicSquare);
+            }
+            while (!stop);
+            
+        printSquare(magicSquare);
+            
+        }
+    
     }
     
 }
